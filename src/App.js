a8eb3d2682accba84775c1be3f82a97916557090
Muhammad Umer <mumer1297@gmail.com>
2020-12-03 13:09:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Quiz App Work In Progress(Progressive Web Application)</h1>
+        <h1>Quiz App PWA Work In Progress(Progressive Web Application)</h1>
       </header>
     </div>
   );
